7de6a10d28964399e9fdae4e50bade5f2f1ed431
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-04 16:34:29 +0500

    Project Completed

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import Coin from "./components/Coin";
 
 function App() {
 	const [listOfCoins, setListOfCoins] = useState([]);
+	const [searchWord, setSearchWord] = useState("");
 	useEffect(() => {
-		Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+		Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
 			(response) => {
 				setListOfCoins(response.data.coins);
 			}
 		);
 	}, []);
+
+	const filteredCoins = listOfCoins.filter((coin) => {
+		return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+	});
+
 	return (
 		<div className="App">
-			<div className="cryptoHeader"></div>
+			<div className="cryptoHeader">
+				<input
+					type="text"
+					placeholder="Bitcoin..."
+					onChange={(e) => {
+						setSearchWord(e.target.value);
+					}}
+				/>
+			</div>
 			<div className="cryptoDisplay">
-				{listOfCoins.map((coin) => (
+				{filteredCoins.map((coin) => (
 					<Coin
 						name={coin.name}
 						icon={coin.icon}
